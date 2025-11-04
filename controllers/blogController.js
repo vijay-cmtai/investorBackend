6096c99844e6a3c82c2da5becfc25c5ff4fade5f
vjzest <vijay.maurya@completrix.com>
2025-11-04 13:43:44 +0530

    commit

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,18 +1,28 @@
 const Blog = require("../models/BlogModel");
 const asyncHandler = require("express-async-handler");
+
+// Public: Gets only published posts
 exports.getAllPosts = asyncHandler(async (req, res) => {
   const posts = await Blog.find({ status: "Published" })
     .populate("author", "name")
     .sort({ createdAt: -1 });
-
   res.status(200).json({ success: true, count: posts.length, data: posts });
 });
+
+// Admin: Gets ALL posts (including drafts)
+exports.getAdminAllPosts = asyncHandler(async (req, res) => {
+  const posts = await Blog.find({})
+    .populate("author", "name")
+    .sort({ createdAt: -1 });
+  res.status(200).json({ success: true, count: posts.length, data: posts });
+});
+
+// Public: Gets a single post by slug
 exports.getPostBySlug = asyncHandler(async (req, res) => {
   const post = await Blog.findOne({ slug: req.params.slug }).populate(
     "author",
     "name"
   );
-
   if (!post) {
     res.status(404);
     throw new Error("Blog post not found");
@@ -20,8 +30,34 @@ exports.getPostBySlug = asyncHandler(async (req, res) => {
   res.status(200).json({ success: true, data: post });
 });
 
+// Admin: Create a new post
 exports.createPost = asyncHandler(async (req, res) => {
-  req.body.author = req.user.id; 
+  req.body.author = req.user.id;
   const post = await Blog.create(req.body);
   res.status(201).json({ success: true, data: post });
+});
+
+// Admin: Update a post by ID
+exports.updatePost = asyncHandler(async (req, res) => {
+  let post = await Blog.findById(req.params.id);
+  if (!post) {
+    res.status(404);
+    throw new Error("Blog post not found");
+  }
+  post = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({ success: true, data: post });
+});
+
+// Admin: Delete a post by ID
+exports.deletePost = asyncHandler(async (req, res) => {
+  const post = await Blog.findById(req.params.id);
+  if (!post) {
+    res.status(404);
+    throw new Error("Blog post not found");
+  }
+  await post.deleteOne();
+  res.status(200).json({ success: true, data: {} });
 });
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -3,13 +3,30 @@ const {
   getAllPosts,
   getPostBySlug,
   createPost,
+  getAdminAllPosts,
+  updatePost,
+  deletePost,
 } = require("../controllers/blogController");
 const { protect, authorize } = require("../middlewares/authMiddleware");
+
 const router = express.Router();
+
+// Admin route to get ALL posts
+router.route("/admin/all").get(protect, authorize("Admin"), getAdminAllPosts);
+
+// Public route to get published posts
 router
   .route("/")
   .get(getAllPosts)
-  .post(protect, authorize("Admin"), createPost); 
+  .post(protect, authorize("Admin"), createPost);
+
+// Public route for single post view
 router.route("/:slug").get(getPostBySlug);
+
+// Admin routes for update and delete by ID
+router
+  .route("/:id")
+  .put(protect, authorize("Admin"), updatePost)
+  .delete(protect, authorize("Admin"), deletePost);
 
 module.exports = router;
